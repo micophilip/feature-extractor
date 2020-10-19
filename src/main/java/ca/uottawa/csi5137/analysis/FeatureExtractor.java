dae6452447227ff55b99e8d00aac0426ad5f7381
mico <micophilip@gmail.com>
2020-10-19 19:59:48 -0400

    fix folder structure and add jcasgen plugin

---
+++ src/main/java/ca/uottawa/csi5137/analysis/FeatureExtractor.java
@@ -0,0 +1,14 @@
+package ca.uottawa.csi5137.analysis;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+import org.uimafit.component.JCasAnnotator_ImplBase;
+
+public class FeatureExtractor extends JCasAnnotator_ImplBase {
+    @Override
+    public void process(JCas aJCas) throws AnalysisEngineProcessException {
+
+    }
+
+
+}
